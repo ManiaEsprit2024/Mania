045c9ecbcbaaccc407477e837502859ca80da21f
syrine1955 <74470806+MeherBenSalem@users.noreply.github.com>
2024-05-04 15:27:20 +0100

    IntegrationModelSE

--- Apps/Backend/controller.js
+++ Apps/Backend/controller.js
@@ -1,4 +1,32 @@
 /**
+ * Predict FICO scores based on user data
+ *
+ * @param {object} req - request object
+ * @param {object} res - response object
+ */
+exports.predictFICOScore = async (req, res) => {
+  const userData = req.body.userData;
+  const userDataProcessed = userData.copy();
+  userDataProcessed['disbursement_date'] = pd.to_datetime(userDataProcessed['disbursement_date']).astype(int);
+  userDataProcessed['optional_stage'] = pd.Categorical(userDataProcessed['optional_stage']).codes;
+
+  try {
+    const predictions = ficoModel.predict(userDataProcessed[list(best_features)]);
+      res.status(200).json({
+      success: true,
+      predictions: predictions
+    });
+  } catch (error) {
+    // Handle errors
+    console.error('Error predicting FICO scores:', error);
+    res.status(500).json({
+      success: false,
+      message: 'Error predicting FICO scores'
+    });
+  }
+};
+
+/**
  * Check the user's information and create a session if the user is authenticated
  *
  * @param {object} req - request object
