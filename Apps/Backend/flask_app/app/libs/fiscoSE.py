b0c09c1e01d3e6b22ed830bf1fec1974b924b44d
SyrineLaabidi <mahourabensalem@gmail.com>
2024-05-11 15:05:24 +0100

    back_V1

--- Apps/Backend/flask_app/app/libs/fiscoSE.py
+++ Apps/Backend/flask_app/app/libs/fiscoSE.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import joblib
 import os
-import numpy as np 
+import numpy as np
+import json
+
 
 label_encoder = joblib.load("app/models/label_encoder.pkl")
 scaler = joblib.load("app/models/standard_scaler.pkl")
@@ -23,26 +25,23 @@ def calculate_risk(probability):
 
     return 'Unknown'
 
-def predict_fico_score(disbursement_date, lender_insurance_premium, jobs_created, optional_revenue_yr_confirmed, optional_stage):
+def predict_fico_score(lender_insurance_premium, jobs_created, optional_revenue_yr_confirmed, optional_stage):
     random_data = pd.DataFrame({
-        'disbursement_date': [disbursement_date],
+        'disbursement_date': 4000,
         'lender_insurance_premium': [lender_insurance_premium],
         'jobs_created': [jobs_created],
         'optional_revenue_yr_confirmed': [optional_revenue_yr_confirmed],
         'optional_stage': [optional_stage]
     })
-    
+
     for column in random_data.select_dtypes(include=['object']):
         random_data[column] = label_encoder.transform(random_data[column])
-    
+
     random_data_scaled = pd.DataFrame(scaler.transform(random_data), columns=random_data.columns)
 
     loan_default_probabilities = xgb_model.predict_proba(random_data_scaled[best_features])[:, 1]
 
     forecast_fico_scores_xg = (300 + (900 - 300) * (1 - loan_default_probabilities)).astype(int)
-    
-    # Randomize the FICO scores
-    forecast_fico_scores_xg += np.random.randint(-50, 51, size=forecast_fico_scores_xg.shape[0])
 
     fico_classes = {
         'LOW': (300, 400),
@@ -67,3 +66,24 @@ def predict_fico_score(disbursement_date, lender_insurance_premium, jobs_created
     })
 
     return forecast_with_classes_xg
+
+from datetime import datetime
+
+def csv_to_json(csv_file):
+    predictions = []
+    try:
+        df = csv_file
+        json_data = []
+        for index, row in df.iterrows():
+            prediction = predict_fico_score(row['lender_insurance_premium'], row['jobs_created'], row['optional_revenue_yr_confirmed'], row['optional_stage'])
+            predictions.append(prediction.iloc[0])
+        predictions_df = pd.DataFrame(predictions)
+        predictions_df.reset_index(drop=True, inplace=True)  
+        result_df = pd.concat([df, predictions_df], axis=1)
+        timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+        output_file = f"Output_{timestamp}.csv"
+        
+        result_df.to_csv(output_file, index=False)
+        return output_file
+    except Exception as e:
+        return {"error": str(e)}
--- Apps/Backend/flask_app/app/routes/fico.py
+++ Apps/Backend/flask_app/app/routes/fico.py
@@ -1,26 +1,25 @@
 from flask import Blueprint, request, jsonify
 from app.libs.fiscoSE import predict_fico_score 
 from app.libs.fiscoBME import predict_fico_score_bme
-
+from app.libs.fiscoSE import csv_to_json 
+import os
 fico = Blueprint('fico', __name__)
-
+import pandas as pd
 @fico.route('/predict_fico_se', methods=['POST'])
 def predict():
     data = request.get_json()
 
-    required_fields = ['disbursement_date', 'lender_insurance_premium', 'jobs_created', 'optional_revenue_yr_confirmed', 'optional_stage']
+    required_fields = ['lender_insurance_premium', 'jobs_created', 'optional_revenue_yr_confirmed', 'optional_stage']
 
     if not all(field in data for field in required_fields):
         return jsonify({'error': 'Missing required fields'}), 400
 
-    disbursement_date = data['disbursement_date']
     lender_insurance_premium = data['lender_insurance_premium']
     jobs_created = data['jobs_created']
     optional_revenue_yr_confirmed = data['optional_revenue_yr_confirmed']
     optional_stage = data['optional_stage']
 
     result = predict_fico_score(
-        disbursement_date,
         lender_insurance_premium,
         jobs_created,
         optional_revenue_yr_confirmed,
@@ -34,7 +33,6 @@ def predict_bme():
     data = request.get_json()
 
     required_fields = [
-        'disbursement_date',
         'revenue',
         'naics_code',
         'lender_type_category',
@@ -44,14 +42,12 @@ def predict_bme():
     if not all(field in data for field in required_fields):
         return jsonify({'error': 'Missing required fields'}), 400
 
-    disbursement_date = data['disbursement_date']
     revenue = data['revenue']
     naics_code = data['naics_code']
     lender_type_category = data['lender_type_category']
     optional_primary_use_of_funds = data['optional_primary_use_of_funds']
 
     result = predict_fico_score_bme(
-        disbursement_date,
         revenue,
         naics_code,
         lender_type_category,
@@ -59,3 +55,27 @@ def predict_bme():
     )
 
     return result.to_json(orient='records'), 200
+
+@fico.route('/predict_fico_dataset', methods=['POST'])
+def predict_dataset():
+    data = request.get_json()
+
+    if 'dataset_name' not in data:
+        return jsonify({'error': 'Dataset name not provided'}), 400
+
+    dataset_name = data['dataset_name']
+
+    dataset_path = os.path.join('app/datasets', f'{dataset_name}')
+    if not os.path.exists(dataset_path):
+        existing_files = os.listdir('app/datasets')
+        return jsonify({'error': f'Dataset "{dataset_name}" not found', 'available_datasets': existing_files}), 404
+    
+    try:
+        dataset = pd.read_csv(dataset_path)
+    except Exception as e:
+        return jsonify({'error': f'Error reading dataset: {str(e)}'}), 500
+
+    predictions = csv_to_json(dataset)
+    return predictions, 200
+
+
